a7cb0e4d9fb6a0e0b8d5a0a7cbd1a9aa92d28726
Hussain Yousof <mhussainyousof@gmail.com>
2025-07-25 15:15:41 +0330

    comment

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  //! This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,7 +22,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: false,
         fontFamily: "Regular"
       ),
-      // home: HomeScreen()
       home: const HomeScreen()
     );
   } 
